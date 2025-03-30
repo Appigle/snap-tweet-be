108f5250cf16f5b0f70b9c4af2d7df56fbcfcdd1
Chenley <chenley@qq.com>
2025-03-29 23:23:04 -0400

    update

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import cors from 'cors';
-import dotenv from 'dotenv';
-import express, { Express } from 'express';
-import connectDB from './config/db';
+import cors from "cors";
+import dotenv from "dotenv";
+import express, { Express } from "express";
+import connectDB from "./config/db";
 
-import authRoutes from './routes/authRoutes';
-import tweetRoutes from './routes/tweetRoutes';
+import authRoutes from "./routes/authRoutes";
+import tweetRoutes from "./routes/tweetRoutes";
 
 dotenv.config();
 
@@ -15,12 +15,12 @@ app.use(express.json());
 
 connectDB(); // Ensure this is mocked in tests
 
-app.use('/api/auth', authRoutes);
-app.use('/api', tweetRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api", tweetRoutes);
 
-if (process.env.NODE_ENV !== 'test') {
-  const PORT: number = parseInt(process.env.PORT || '5000', 10);
-  app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`));
-}
+// if (process.env.NODE_ENV !== 'test') {
+//   const PORT: number = parseInt(process.env.PORT || '5000', 10);
+//   app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`));
+// }
 
 export default app; // Ensure the app is exported for testing
